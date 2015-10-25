2fc995303ab06ea1633b7c40cc4ddd138ee8b86c
akasiedu <akasiedu@gmail.com>
2015-10-25 21:23:06 +0000

    Styled ngo profile with dashboard

--- client/templates/Dashboard/dashboardHelper.js
+++ client/templates/Dashboard/dashboardHelper.js
@@ -3,5 +3,9 @@ Template.dashboardLayout.helpers({
 		if(Meteor.user().profile.companyType == "ngo"){
 			return true;
 		}
+	},
+	'ngoProfile': function(){
+		return Ngo.findOne({ngoCompanyOwner: Meteor.userId()})
 	}
 });
+
