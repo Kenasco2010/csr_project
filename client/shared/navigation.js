07028e85d0f59e710f1ad210e44faa35c8304344
akasiedu <akasiedu@gmail.com>
2015-10-28 10:03:53 +0000

    style ngo profile page

---
+++ client/shared/navigation.js
@@ -0,0 +1,7 @@
+// Template.navigation.events({
+// 	'click #dashboard': function () {
+// 		if(Meteor.user().profile.companyType === "ngo"){
+// 			Router.go('showNGOProfile')
+// 		}
+// 	}
+// });
