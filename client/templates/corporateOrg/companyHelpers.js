26864bc7e28962bc642b7fc706199e3f7f845aeb
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-11-01 23:55:53 +0000

    worked on content of the page

--- client/templates/corporateOrg/companyHelpers.js
+++ client/templates/corporateOrg/companyHelpers.js
@@ -19,31 +19,41 @@ Template.companyProjectInterest.helpers({
         if (companyProjectScope == undefined) {
             return null
         }
-        console.log(companyProjectScope);
+
         var projectScope = Projects.find({scope: {$in: companyProjectScope}}).fetch();
         return projectScope;
 
-        console.log("hi");
-        console.log(projectScope);
        // var hasData = _.contains(companyProjectScope.project);
         //console.log(hasData);
 
-        if (projectScope) {
-            console.log("true");
-            return "Company interest Project List"
-        } else {
-            console.log("false");
-            return "No Company Project Interest"
-        }
+        //if (projectScope) {
+        //    console.log("true");
+        //    return "Company interest Project List"
+        //} else {
+        //    console.log("false");
+        //    return "No Company Project Interest"
+        //}
     }
+    //All matching Ngo for a company
+    //companyNgoInterest: function () {
+    //    var loggedInCompany = Meteor.user() && Meteor.user().profile.companyType === "corporateOrganization";
+    //    var companyProjectScope = CorpOrg.findOne({companyOwner: Meteor.userId()}).project;
+    //    if (companyProjectScope == undefined) {
+    //        return null
+    //    }
+    //
+    //    var companyNgoInterest = Ngo.find({focus: {$in: companyProjectScope}}).fetch();
+    //    return companyNgoInterest;
+    //
+    //}
 });
 
-// Template.corporateOrgProjectsOnly.helpers({
-//   corporateProjectOnly: function(){
-// var companyType = Meteor.user().profile.companyType
-//   return Projects.find({companyType: companyType}).fetch();
-//     console.log(companytype);
-//     return companytype;
-//   }
-// });
+ Template.myCompanyProjects.helpers({
+   myCompanyProject: function(){
+ var companyType = Meteor.user().profile.companyType;
+   return Projects.find({companyType: companyType}).fetch();
+     console.log(companytype);
+     return companytype;
+   }
+ });
 
